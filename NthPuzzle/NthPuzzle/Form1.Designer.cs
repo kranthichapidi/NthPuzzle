9f4c412cfa6b61247d24ebf64e659f7e06557f34
kranthichapidi <kranthichapidi@live.com>
2017-10-26 13:01:08 -0400

    Add files via upload

---
+++ NthPuzzle/NthPuzzle/Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace NthPuzzle
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.TXTRowsAndColumns = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.TLPGameLayout = new System.Windows.Forms.TableLayoutPanel();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1265, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newToolStripMenuItem,
+            this.openToolStripMenuItem,
+            this.saveToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // newToolStripMenuItem
+            // 
+            this.newToolStripMenuItem.Name = "newToolStripMenuItem";
+            this.newToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.newToolStripMenuItem.Text = "New";
+            this.newToolStripMenuItem.Click += new System.EventHandler(this.newToolStripMenuItem_Click);
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.saveToolStripMenuItem.Text = "Save";
+            this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // TXTRowsAndColumns
+            // 
+            this.TXTRowsAndColumns.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TXTRowsAndColumns.Location = new System.Drawing.Point(759, 34);
+            this.TXTRowsAndColumns.Name = "TXTRowsAndColumns";
+            this.TXTRowsAndColumns.Size = new System.Drawing.Size(100, 38);
+            this.TXTRowsAndColumns.TabIndex = 1;
+            this.TXTRowsAndColumns.TextChanged += new System.EventHandler(this.TXTRowsAndColumns_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(150, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(567, 31);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Please enter number of rows and columns:";
+            // 
+            // TLPGameLayout
+            // 
+            this.TLPGameLayout.ColumnCount = 2;
+            this.TLPGameLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.TLPGameLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.TLPGameLayout.Location = new System.Drawing.Point(290, 88);
+            this.TLPGameLayout.Name = "TLPGameLayout";
+            this.TLPGameLayout.RowCount = 2;
+            this.TLPGameLayout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.TLPGameLayout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.TLPGameLayout.Size = new System.Drawing.Size(900, 900);
+            this.TLPGameLayout.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1265, 697);
+            this.Controls.Add(this.TLPGameLayout);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.TXTRowsAndColumns);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem newToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.TextBox TXTRowsAndColumns;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TableLayoutPanel TLPGameLayout;
+    }
+}
+
---
+++ NthPuzzle/NthPuzzle/Form1.cs
@@ -0,0 +1,292 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NthPuzzle
+{
+    public partial class Form1 : Form
+    {
+        bool ISLOADGAME = false;
+        int rows = 4, columns = 4;
+        Random r;
+        List<int> inputList = new List<int>();
+        string Inputs;
+        string compareString = "";
+        string compareStringNew = "";
+        string currInputs = "";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void newToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(TXTRowsAndColumns.Text))
+                rows = columns = Convert.ToInt32(TXTRowsAndColumns.Text);
+            else
+                MessageBox.Show("You havent entered any input. So, This game is going to start with 4 rows and 4 columns.");
+            ClearLayout();
+            FillInputs();
+            CreatingControles();
+            CreateInputs();
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.InitialDirectory = "c:\\";
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                compareString = File.ReadAllText(ofd.FileName).Replace("\r", string.Empty).Replace("\t", string.Empty).Replace("\n", string.Empty);
+                string[] rowsColumns = compareString.Split(',');
+                rows = columns = Convert.ToInt32(Math.Sqrt(rowsColumns.Count()));
+                TXTRowsAndColumns.Text = rows.ToString();
+                ClearLayout();
+                FillInputs();
+                CreatingControles(compareString);
+                CreateInputs();
+                //startGame(tagging);
+            }
+        }
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Stream myStream;
+            SaveFileDialog save = new SaveFileDialog();
+            save.InitialDirectory = "c:\\";
+            if (save.ShowDialog() == DialogResult.OK)
+            {
+                if ((myStream = save.OpenFile()) != null)
+                {
+                    myStream.Close();
+                    System.IO.File.WriteAllLines(save.FileName, new[] { compareString });
+                }
+            }
+        }
+        public void ClearLayout()
+        {
+            TLPGameLayout.Controls.Clear();
+            TLPGameLayout.RowStyles.Clear();
+            TLPGameLayout.ColumnStyles.Clear();
+            TLPGameLayout.ColumnCount = columns;
+            TLPGameLayout.RowCount = rows;
+            for (int i = 0; i < rows; i++)
+            {
+                TLPGameLayout.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 25));
+                TLPGameLayout.RowStyles.Add(new RowStyle(SizeType.Percent, 25));
+            }
+        }
+        public void FillInputs()
+        {
+            int length = rows * columns;
+            Inputs = "";
+            for (int i = 0; i < length; i++)
+            {
+                inputList.Add(i);
+                if (i == length - 1)
+                    Inputs += i.ToString();
+                else
+                    Inputs += i.ToString() + ",";
+            }
+        }
+        public void CreatingControles()
+        {
+            string[] inputs = compareString.Split(',');
+            currInputs = "";
+            int inputIndex = 0;
+            int k = 0;
+            r = new Random();
+            for (int i = 0; i < TLPGameLayout.RowCount; i++)
+            {
+                for (int j = 0; j < TLPGameLayout.ColumnCount; j++)
+                {
+                    Button controleButton = new Button();
+                    TLPGameLayout.Controls.Add(controleButton, i, j);
+                    controleButton.Dock = DockStyle.Fill;
+                    controleButton.Click += ControleButton_Click;
+                    controleButton.Font = new Font("Times New Roman", 35);
+                    if (ISLOADGAME)
+                    {
+                        currInputs = inputs[inputIndex];
+                        inputIndex++;
+                    }
+                    else
+                    {
+                        k = r.Next(0, inputList.Count);
+                        currInputs = inputList[k].ToString();
+                    }
+                    controleButton.Text = currInputs;
+                    if (currInputs != "0")
+                        controleButton.Visible = true;
+                    else
+                        controleButton.Visible = false;
+                    if (!ISLOADGAME)
+                        inputList.RemoveAt(k);
+                }
+
+            }
+        }
+        public void CreatingControles(string fromFile)
+        {
+            string[] inputs = fromFile.Split(',');
+            int inputIndex = 0;
+            for (int i = 0; i < TLPGameLayout.RowCount; i++)
+            {
+                for (int j = 0; j < TLPGameLayout.ColumnCount; j++)
+                {
+                    Button controleButton = new Button();
+                    TLPGameLayout.Controls.Add(controleButton, j, i);
+                    controleButton.Dock = DockStyle.Fill;
+                    controleButton.Click += ControleButton_Click;
+                    controleButton.Font = new Font("Times New Roman", 35);
+                    currInputs = inputs[inputIndex];
+                    inputIndex++;
+                    controleButton.Text = currInputs;
+                    if (currInputs != "0")
+                        controleButton.Visible = true;
+                    else
+                        controleButton.Visible = false;
+                }
+
+            }
+        }
+        public void CreateInputs()
+        {
+            compareString = "";
+            compareStringNew = 0.ToString();
+            int count = 0;
+            for (int i = 0; i < TLPGameLayout.RowCount; i++)
+            {
+                for (int j = 0; j < TLPGameLayout.ColumnCount; j++)
+                {
+                    Button button = (Button)TLPGameLayout.GetControlFromPosition(j, i);
+                    compareString += button.Text;
+                    if (count < rows * columns)
+                    {
+                        compareString += ",";
+                        count++;
+                    }
+                }
+            }
+            compareStringNew = 0 + "," + compareString;
+        }
+        private void TXTRowsAndColumns_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        public void Swap(Button currButton, Button nextButton)
+        {
+            string text = nextButton.Text.ToString();
+            nextButton.Text = currButton.Text;
+            nextButton.Text = currButton.Text;
+            nextButton.Visible = true;
+            currButton.Text = text;
+            currButton.Visible = false;
+        }
+        public void winningDecition()
+        {
+            if ((String.Compare(this.Inputs, this.compareString) == 0) || (this.compareStringNew.Contains(this.Inputs)))
+            {
+                string message = "Game is over.\nTry again?";
+                string caption = "The end";
+                MessageBoxButtons buttons = MessageBoxButtons.YesNo;
+                DialogResult result;
+                result = MessageBox.Show(message, caption, buttons, MessageBoxIcon.Information);
+                if (result == System.Windows.Forms.DialogResult.Yes)
+                {
+                    inputList = new List<int>();
+                    compareString = "";
+                    compareStringNew = "";
+                    currInputs = "";
+                    ClearLayout();
+                    FillInputs();
+                    CreatingControles();
+                    CreateInputs();
+                }
+                else
+                    this.Close();
+            }
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Stream myStream;
+            SaveFileDialog save = new SaveFileDialog();
+            save.InitialDirectory = "c:\\";
+            if (save.ShowDialog() == DialogResult.OK)
+            {
+                if ((myStream = save.OpenFile()) != null)
+                {
+                    myStream.Close();
+                    System.IO.File.WriteAllLines(save.FileName, new[] { compareString });
+                    this.Close();
+                }
+            }
+        }
+
+        private void ControleButton_Click(object sender, EventArgs e)
+        {
+            int column = TLPGameLayout.GetColumn(sender as Button);
+            int row = TLPGameLayout.GetRow(sender as Button);
+            Button currButton = (sender as Button);
+            if (row != 0)
+            {
+                //Top
+                Button b = new Button();
+                b = (Button)TLPGameLayout.GetControlFromPosition(column, row - 1);
+                if (b.Visible == false)
+                {
+                    Swap(currButton, b);
+                    CreateInputs();
+                    winningDecition();
+                }
+            }
+            if (row != rows - 1)
+            {
+                //Bottom
+                Button b = new Button();
+                b = (Button)TLPGameLayout.GetControlFromPosition(column, row + 1);
+                if (b.Visible == false)
+                {
+                    Swap(currButton, b);
+                    CreateInputs();
+                    winningDecition();
+                }
+            }
+            if (column != 0)
+            {
+                //Left
+                Button b = new Button();
+                b = (Button)TLPGameLayout.GetControlFromPosition(column - 1, row);
+                if (b.Visible == false)
+                {
+                    Swap(currButton, b);
+                    CreateInputs();
+                    winningDecition();
+                }
+            }
+            if (column != column - 1)
+            {
+                //Right
+                Button b = new Button();
+                if (column == column - 1)
+                    b = (Button)TLPGameLayout.GetControlFromPosition(column, row);
+                else
+                    b = (Button)TLPGameLayout.GetControlFromPosition(column + 1, row);
+                if (b != null && b.Visible == false)
+                {
+                    Swap(currButton, b);
+                    CreateInputs();
+                    winningDecition();
+                }
+            }
+        }
+    }
+}
